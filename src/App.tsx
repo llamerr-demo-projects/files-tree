e3084cbf5f556dfa2d6ae409219ffad78c36b121
Alexey Lukin <llamerr@gmail.com>
2024-09-13 14:07:32 +0300

    add solution without antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
 import "./styles.css";
 import { Button } from "antd";
-import React from "react";
+import { useEffect, useState } from "react";
 
 import { FolderTree } from "./components/FolderTree";
+import { sourceApi } from "./api/source";
+import { processFiles, processFolders, RecursiveFolderTree } from "./components/FolderTree.types";
 
 export default function App() {
+  const [foldersList, setFoldersList] = useState<RecursiveFolderTree>([]);
+  const [toggleAllState, setToggleAllState] = useState<boolean|null>(null);
+
+  useEffect(() => {
+    const fetchFolders = async () => {
+      return await sourceApi.getFolders()
+    }
+    const fetchFiles = async () => {
+      return await sourceApi.getFiles();
+    }
+
+    Promise.all([fetchFolders(), fetchFiles()]).then(([folders, files]) => {
+      const processedFolders = processFolders(folders);
+      processFiles(processedFolders, files);
+      setFoldersList(processedFolders)
+    });
+  }, []);
+
+  const toggleAll = () => {
+    const state = toggleAllState === null? true : !toggleAllState;
+    setToggleAllState(state);
+  };
+
+  const resetTogglaAll = () => {
+    setToggleAllState(null);
+  }
+  
   return (
     <div className="App">
       <Button>ANTD </Button>
       <tr />
-      <FolderTree />
+      <button 
+        onClick={toggleAll} 
+        className={toggleAllState !== null ? "" : "inactive"}
+      >
+        Toggle All {toggleAllState ? "▼" : "▶"}
+      </button>
+      <FolderTree folders={foldersList} onToggleReset={resetTogglaAll} toggleAllState={toggleAllState} className="folder-tree" />
     </div>
   );
 }
--- src/components/FolderTree.tsx
+++ src/components/FolderTree.tsx
@@ -1,20 +1,56 @@
 import React, { useEffect, useState } from "react";
-import { sourceApi } from "../api/source";
+import { RecursiveFolderTree } from "./FolderTree.types";
+
+import "./FolderTree.styles.css";
 
 export interface TreeProps {
-
+  folders: RecursiveFolderTree;
+  className?: string;
+  toggleAllState?: boolean | null;
+  onToggleReset: () => void;
 }
 
-export const FolderTree = React.FC<TreeProps>(props) => {
-  const [foldersList, setFoldersList] = useState([]);
+export const FolderTree: React.FC<TreeProps> = ({ folders, className, toggleAllState = null, onToggleReset }) => {
+  const [expanded, setExpanded] = useState<Array<boolean>>(folders.map(() => false));
 
   useEffect(() => {
-    setFoldersList(sourceApi.getFolders());
-  }, []);
+    setExpanded(folders.map(() => false));
+  }, [folders]);
+
+  useEffect(() => {
+    if (toggleAllState !== null) {
+      setExpanded((prev) => {
+        const copy = [...prev.map(() => toggleAllState)];
+        return copy;
+      });
+    }
+  }, [toggleAllState]);
+
+  const toggle = (index: number) => {
+    onToggleReset();
+    setExpanded((prev) => {
+      const copy = [...prev];
+      copy[index] = !prev[index];
+      return copy;
+    });
+  };
+
   return (
-    <ul>
-      {foldersList.map()}
-      <button>+</button>
-    </ul>
+    <>
+      <ul className={className}>
+        {folders.map((folder, index) => (
+          <li key={folder.id}>
+            <button onClick={() => toggle(index)}>{expanded[index] ? "▼" : "▶"}</button>
+            {folder.name}{' '}
+            {(expanded[index] && folder.folders.length > 0) && (
+              <FolderTree folders={folder.folders} toggleAllState={toggleAllState} onToggleReset={onToggleReset} />
+            )}
+            {(expanded[index] && folder.files.length > 0) && (
+              <ul>{folder.files.map((file) => <li key={file.id}>{file.name}</li>)}</ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
